28ec717b88d0441c2565bc5781a197048c42caef
Ebikakpou <Ebikakpou@gmail.com>
2026-02-13 02:14:44 +0100

    Initialize Django project with core app

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HomePageTest(TestCase):
+
+    def test_home_page_returns_200(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_content(self):
+        response = self.client.get("/")
+        self.assertContains(response, "Production-Grade Django CI/CD Pipeline")
